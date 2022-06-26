6803a49028c60be16f4c62837df0e1cca1d4629c
Lê Huy <huykvp@gmail.com>
2022-06-26 11:01:25 +0700

    sua install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 pip install -r requirements.txt
 pip install pandas torch scipy pylsd lsd ocrd-fork-pylsd==0.0.3 torchvision pyyaml tqdm sklearn pycocotools Polygon pyclipper scikit-image einops
 pip install gdown==4.4.0 webcolors transformers
-gdown --fuzzy 'https://drive.google.com/uc?id=1-Nj8TSM_eqZDZzRArZjWPcVCtl1l2uQP' -O '/content/main/weights/PANNet_best_map.pth'
-gdown --fuzzy 'https://drive.google.com/uc?id=1qpXp_-digz2HPTGY_GPdwstzGLhjC_ot' -O '/content/main/weights/transformerocr.pth'
+gdown --fuzzy 'https://drive.google.com/uc?id=1-Nj8TSM_eqZDZzRArZjWPcVCtl1l2uQP' -O '/weights/PANNet_best_map.pth'
+gdown --fuzzy 'https://drive.google.com/uc?id=1qpXp_-digz2HPTGY_GPdwstzGLhjC_ot' -O '/weights/transformerocr.pth'
 ## --> Run file main.py
