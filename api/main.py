3274d4ff3604343bbc62a730959b309c8dd56026
Lê Huy <huykvp@gmail.com>
2022-06-30 12:26:47 +0700

    update

--- api/main.py
+++ api/main.py
@@ -12,7 +12,6 @@ async def perform_ocr(file: UploadFile = File(...)):
 
     texts = ocr_end2end.img_to_txt(file_location)
     link_sound = soundlink_end2end.txt_to_wav(texts)
-    #link_sound = texts
     print(link_sound)
 
     return{'link': link_sound}
